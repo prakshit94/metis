244b6fe66a9e36c6cbaeab199da4d78b7bc4257c
Your Name <bugfix@gmail.com>
2026-08-15 13:04:23 +0530

    clear code

--- test_relation.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace App\Modules\Inventory\Models;
-use Illuminate\Database\Eloquent\Model;
-// We will test if the relationship works in a closure
--- test_rmb.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::create('/api/customers/8/addresses', 'POST');
-$response = $kernel->handle($request);
-echo $response->getContent();
