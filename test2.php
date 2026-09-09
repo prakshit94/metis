2d127d6df8d806a36b17d17e84945167688b5bd2
Your Name <bugfix@gmail.com>
2026-09-09 15:31:15 +0530

    updated validation

--- test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$paginator = new \Illuminate\Pagination\LengthAwarePaginator([1, 2, 3], 3, 15);
-echo json_encode(['data' => $paginator]);
