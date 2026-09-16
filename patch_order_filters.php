5e7e18281007ce678290dd9bb9de6f47f4eb6e7d
Your Name <you@example.com>
2026-09-16 23:38:01 +0530

    fetures implemented order import

--- patch_order_filters.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = 'app/Modules/Orders/Controllers/OrderController.php';
-$content = file_get_contents($file);
-
-// We need to move the filter block into a protected function and call it from index and bulkExport
-// This is somewhat complex via string replacement. Let's do it using regex.
